f8a593f11878085396d65bdc85d97eeaa7c4de7e
olayinka <koleajeolayinka@gmail.com>
2022-05-25 17:03:34 +0100

    Exercises

--- Test/ArrayList.java
+++ Test/ArrayList.java
@@ -7,58 +7,90 @@ import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
 
 
 public class ArrayList {
-    public static int[][] listOfArray = {{20,30,50},
+    public static int[][] listOfArray = {{20, 30, 50},
             {20}};
+
     @Test
-    public void Test_Array_Can_Be_Create(){
+    public void Test_Array_Can_Be_Create() {
         ArrayL arrayList = new ArrayL();
         assertNotNull(arrayList);
 
     }
+
     @Test
-    public void Check_If_Array_List_Is_Not_Empty_Test(){
+    public void Check_If_Array_List_Is_Not_Empty_Test() {
         ArrayL arrayList = new ArrayL();
-        assertTrue( arrayList.CheckArray());
+        assertTrue(arrayList.CheckArray());
 
     }
 
 
     @Test
-    public void Array_List_Can_Add(){
+    public void Array_List_Can_Add() {
         ArrayL arrayList = new ArrayL();
         assertEquals(100, arrayList.getArrayListAddition(0));
 
     }
+
     @Test
-    public void Test_Object_Can_Be_Put_In(){
+    public void Test_Object_Can_Be_Put_In() {
         ArrayL arrayList = new ArrayL();
         assertArrayEquals(new Object[][]{{"q", "q", "q", "q"},
-                                         {" ", " ", " ", " "} }, arrayList.CreateObject());
+                {" ", " ", " ", " "}}, arrayList.CreateObject());
 
     }
+
     @Test
-    public void Test_One_Object_Can_Added(){
+    public void Test_One_Object_Can_Added() {
         ArrayListItem arrayList = new ArrayListItem("rice", "Spaghetti", "Beans");
         arrayList.addItem("bread", "egg");
         assertArrayEquals(new Object[][]{{"rice", "Spaghetti", "Beans", "bread", null}}, arrayList.getOneItem());
 
 
-
-
     }
+
     @Test
-    public void Test_One_Object_Can_Be_Removed(){
+    public void Test_One_Object_Can_Be_Removed() {
         ArrayListItem arrayList = new ArrayListItem("rice", "Spaghetti", "Beans");
-        arrayList.addItem("bread","egg");
+        arrayList.addItem("bread", "egg");
         arrayList.removeItem(0, 0);
-        assertArrayEquals(new Object[][]{{null, "Spaghetti", "Beans", "bread",null}}, arrayList.getRemoveItem());
+        assertArrayEquals(new Object[][]{{null, "Spaghetti", "Beans", "bread", null}}, arrayList.getRemoveItem());
 
     }
-@Test
-    public void Test_Two_Object_Can_Be_Added(){
-    ArrayListItem arrayList = new ArrayListItem("rice", "Spaghetti", "Beans");
-    arrayList.addItem("bread", "Egg");
-    assertArrayEquals(new Object[][]{{"rice", "Spaghetti", "Beans", "bread", "Egg"}}, arrayList.getOneMoreItem());
+
+    @Test
+    public void Test_Two_Object_Can_Be_Added() {
+        ArrayListItem arrayList = new ArrayListItem("rice", "Spaghetti", "Beans");
+        arrayList.addItem("bread", "Egg");
+        assertArrayEquals(new Object[][]{{"rice", "Spaghetti", "Beans", "bread", "Egg"}}, arrayList.getOneMoreItem());
+    }
+
+
+    @Test
+    public void Test_Set_Can_Be_Created() {
+        ArrayListSet set = new ArrayListSet();
+        assertNotNull(set);
+
+    }
+
+    @Test
+    public void Test_Two_Set_Can_Be_Created() {
+        ArrayListSet arraySet = new ArrayListSet();
+        arraySet.SetFirstSet(2, 4, 3, 2, 5);
+        arraySet.SetSecondSet(2, 2, 1, 5, 5);
+        assertArrayEquals(new Object[]{2, 4, 3, 2, 5}, arraySet.getFirstSet());
+        assertArrayEquals(new Object[]{2, 2, 1, 5, 5}, arraySet.getSecondSet());
+
+
+    }
+
+    @Test
+    public void Test_The_Different_Is_Correct() {
+        ArrayListSet arraySet = new ArrayListSet();
+        arraySet.SetFirstSet(2, 4, 3, 2, 5);
+        arraySet.SetSecondSet(2, 2, 1, 5, 5);
+
+
     }
 
 
---
+++ src/ArrayListSet.java
@@ -0,0 +1,61 @@
+public class ArrayListSet {
+ Object[] FirstSet = {null,null,null,null,null};
+Object[] SecondSet = {null,null,null,null,null};
+    public int FirstSet0;
+    public int FirstSet1;
+    public int FirstSet2;
+    public int FirstSet3;
+    public int FirstSet4;
+
+    public int SecondSet0;
+    public int SecondSet1;
+    public int SecondSet2;
+    public int SecondSet3;
+    public int SecondSet4;
+
+    public void SetFirstSet(int FirstSet0, int FirstSet1, int FirstSet2, int FirstSet3, int FirstSet4) {
+        this.FirstSet0 = FirstSet0;
+        this.FirstSet1 = FirstSet1;
+        this.FirstSet2 = FirstSet2;
+        this.FirstSet3 = FirstSet3;
+        this.FirstSet4 = FirstSet4;
+
+    }
+    public void SetSecondSet(int SecondSet0, int SecondSet1, int SecondSet2, int SecondSet3, int SecondSet4) {
+        this.SecondSet0 = SecondSet0;
+        this.SecondSet1 = SecondSet1;
+        this.SecondSet2 = SecondSet2;
+        this.SecondSet3 = SecondSet3;
+        this.SecondSet4 = SecondSet4;
+
+    }
+    public Object[] getFirstSet() {
+        for (int i = 0; i < FirstSet.length; i++) {
+                if (FirstSet[i] == FirstSet[0]) FirstSet[0] = FirstSet0;
+                if (FirstSet[i] == FirstSet[1]) FirstSet[1] = FirstSet1;
+                if (FirstSet[i] == FirstSet[2]) FirstSet[2] = FirstSet2;
+                if (FirstSet[i] == FirstSet[3]) FirstSet[3] = FirstSet3;
+                if (FirstSet[i] == FirstSet[4]) FirstSet[4] = FirstSet4;
+
+            }
+
+        return FirstSet;
+
+    }
+
+    public Object[] getSecondSet() {
+        for (int i = 0; i < SecondSet.length; i++) {
+            if (SecondSet[i] == SecondSet[0]) SecondSet[0] = SecondSet0;
+            if (SecondSet[i] == SecondSet[1]) SecondSet[1] = SecondSet1;
+            if (SecondSet[i] == SecondSet[2]) SecondSet[2] = SecondSet2;
+            if (SecondSet[i] == SecondSet[3]) SecondSet[3] = SecondSet3;
+            if (SecondSet[i] == SecondSet[4]) SecondSet[4] = SecondSet4;
+
+        }
+
+        return SecondSet;
+    }
+
+
+
+}
